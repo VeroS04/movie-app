5691c2578c582878db1089377805d4342d358b08
Vero04 <veronica.c.segovia@gmail.com>
2023-03-13 13:59:57 -0300

    se agrega endpoint en el archivo index.ts de la carpeta constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,5 +3,6 @@ export const img_url = "http://image.tmdb.org/t/p/original";
 export const endpoints = {
   upcoming_movie: "/movie/upcoming",
   latest_movie: "/movie/latest",
-  top_movie: "/movie/top_rated"
+  top_movie: "/movie/top_rated",
+  popular_movie: "/movie/popular"
 };
