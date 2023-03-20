f4b9f1185bf6514bca269368abc0b2428cc5834f
Vero04 <veronica.c.segovia@gmail.com>
2023-03-20 12:23:34 -0300

    resolviendo conflictos

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,11 +1,4 @@
 export { BannerCarousel } from './banner-carrousell';
-<<<<<<< HEAD
 export { GridMovies } from './gridMovies';
 export { CardsMovie } from './movieCards'
-=======
-export { PopularesList } from './populares-home';
-export { PopularesPage } from './populares-page';
-export { CardsMovie } from './movie-cards';
-export { TopList } from './top-home';
->>>>>>> e58e7ac4f932fc525fe1d0926cc0709589433f4d
 
--- src/components/common/top-home/type.ts
+++
@@ -1,4 +0,0 @@
-export type MovieTop = {
-    movies: [] | any
-    text?: string
-  }
